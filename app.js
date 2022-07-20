7eb589f263c43b3acec9f6cf1f9e288c02b46f48
chocoflames <ezevalentine107@gmail.com>
2022-07-20 14:34:00 +0100

    adding changes in css

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
-const txtInput = document.querySelector(".inputs");
+const txtInput = document.querySelector(".inputs input");
+const checkBtn = document.querySelector(".inputs button");
 
 txtInput.addEventListener("keyup", () => {
-    console.log(txtInput.value);
+    //removing spaces & all special character from entered value
+    let filterInput = txtInput.value.replace(/[^A-z0-9]/ig, "")
+    if(filterInput){
+        return checkBtn.classList.add("active")
+    }
+    return checkBtn.classList.remove("active")
+
 })
