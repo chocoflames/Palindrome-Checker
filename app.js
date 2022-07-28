562aa3eadd91a33d648f4446d37cf2fe78bb3c79
chocoflames <ezevalentine107@gmail.com>
2022-07-28 17:57:59 +0100

    completed palindrome checker project

--- app.js
+++ app.js
@@ -1,11 +1,22 @@
 const txtInput = document.querySelector(".inputs input");
 const checkBtn = document.querySelector(".inputs button");
+const infoTxt = document.querySelector(".info-txt");
+let filterInput;
 
-checkBtn.addEventListener
+checkBtn.addEventListener("click", () => {
+    // splitting user input character, reversing them,
+    // and joining them in a single word
+    let reverseInput = filterInput.split("").reverse().join("");
+    infoTxt.style.display = "block";
+    if(filterInput != reverseInput){
+        return infoTxt.innerHTML = `No, <span>'${txtInput.value}</span> isn't a palindrome`;
+    }
+    infoTxt.innerHTML = `Yes, <span>'${txtInput.value}</span> is a palindrome`;
+})
 
 txtInput.addEventListener("keyup", () => {
     //removing spaces & all special character from entered value
-    let filterInput = txtInput.value.replace(/[^A-z0-9]/ig, "")
+    filterInput = txtInput.value.toLowerCase().replace(/[^A-z0-9]/ig, "")
     if(filterInput){
         return checkBtn.classList.add("active")
     }
